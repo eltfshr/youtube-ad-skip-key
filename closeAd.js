bf19cadbe24057ad544149909bea2936b1dafdee
eltfshr <puntf2@gmail.com>
2024-04-20 19:48:01 +0700

    Use css selector

--- closeAd.js
+++ closeAd.js
@@ -16,25 +16,28 @@ document.addEventListener('keypress', (e) => {
     return
   }
 
-  let countDown = document.getElementsByClassName('ytp-ad-preview-container')[0] || null
-  let pie = document.getElementsByClassName('ytp-ad-timed-pie-countdown-container')[0] || null
+  // countDown here is the 5 seconds countdown to skip
+  let countDown = document.querySelectorAll('div[class*="preview"][class*="ad"]:not([class*="player"])')[0] || null
+
+  // pie is the upper right circle pie
+  let pie = document.querySelectorAll('div[class*="pie"][class*="countdown"]')[0] || null
   let button
 
-  if ((countDown && countDown.getAttribute('style').includes('display: none;')) || pie) {
-    button = document.getElementsByClassName('ytp-skip-ad-button')[0] || document.getElementsByClassName('ytp-ad-skip-button-icon-modern')[0] || document.getElementsByClassName('ytp-ad-skip-button-modern ytp-button')[0] || document.querySelectorAll('button[class*="skip"]')[0] || document.querySelectorAll('[id^="skip-button"]')[1].childNodes[0].childNodes[0] || document.getElementsByClassName('ytp-ad-skip-button ytp-button')[0] || null
-  } else {
-    button = document.getElementsByClassName('ytp-ad-overlay-close-container')[0] || null
+  if ((countDown && (countDown.getAttribute('style').match(/display.*none/g)) !== null) || pie) { // normal skip ad
+    button = document.querySelectorAll('button[class*="skip"][class*="ad"]')[0] || document.querySelectorAll('[id^="skip-button"]')[1].childNodes[0].childNodes[0] || null
+  } else { // bottom overlay square
+    button = document.querySelectorAll('button[class*="overlay"][class*="ad"][class*="close"]')[0] || document.getElementsByClassName('ytp-ad-overlay-close-container')[0] || null
   }
 
-  if (document.querySelector('ytd-button-renderer#dismiss-button') && !document.querySelector('ytd-button-renderer#dismiss-button').parentElement.parentElement.parentElement.parentElement.getAttribute('style').includes('display: none;')) {
-    button = document.querySelector('ytd-button-renderer#dismiss-button') || null
+  // close youtube premium ads
+  let premium = document.querySelector('yt-button-renderer[id*="dismiss"][id*="button"]') || document.querySelector('ytp-button-renderer[id*="dismiss"][id*="button"]') || document.querySelector('ytd-button-renderer[id*="dismiss"][id*="button"]') || null
+
+  if (premium && !premium.parentElement.parentElement.parentElement.parentElement.getAttribute('style').match(/display.*none/g) !== null) {
+    button = premium || null
   }
 
-  if (document.querySelector('yt-button-renderer#dismiss-button') && !document.querySelector('yt-button-renderer#dismiss-button').parentElement.parentElement.parentElement.parentElement.getAttribute('style').includes('display: none;')) {
-    button = document.querySelector('yt-button-renderer#dismiss-button') || null
-  }
-
-  if (button && e.target.tagName != 'INPUT' && e.target.className != 'style-scope yt-formatted-string') {
+  // skip ad if focus is not on input boxes
+  if (button && e.target.tagName != 'INPUT' && e.target.className.match(/style.*scope.*formatted.*string/g) === null) {
     button.click()
   }
 })
