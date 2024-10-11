e54d220f32ee69f81a25903dba54ff66ad95cfa6
eltfshr <puntf2@gmail.com>
2024-10-12 00:14:12 +0700

    adjust to new hidden button

--- closeAd.js
+++ closeAd.js
@@ -31,12 +31,14 @@ document.addEventListener('keypress', (e) => {
   // bottom overlay square
   button = document.querySelectorAll('button[class*="overlay"][class*="ad"][class*="close"]')[0] || document.getElementsByClassName('ytp-ad-overlay-close-container')[0] || null
 
-  if (premium && !premium.parentElement.parentElement.parentElement.parentElement.getAttribute('style').match(/display.*none/g) !== null) {
-    button = premium || null
+  let isButtonReady = document.querySelectorAll('button[class*="skip"][class*="ad"]')[0] && document.querySelectorAll('button[class*="skip"][class*="ad"]')[0].getAttribute('style').match(/display.*none/g) === null
+
+  if ((countDown && (countDown.getAttribute('style').match(/display.*none/g)) !== null) || pie || survey || isButtonReady) { // normal skip ad
+    button = document.querySelectorAll('button[class*="skip"][class*="ad"]')[0] || document.querySelectorAll('[id^="skip-button"]')[1].childNodes[0].childNodes[0] || null
   }
 
-  if ((countDown && (countDown.getAttribute('style').match(/display.*none/g)) !== null) || pie || survey) { // normal skip ad
-    button = document.querySelectorAll('button[class*="skip"][class*="ad"]')[0] || document.querySelectorAll('[id^="skip-button"]')[1].childNodes[0].childNodes[0] || null
+  if (premium && !premium.parentElement.parentElement.parentElement.parentElement.getAttribute('style').match(/display.*none/g) !== null) {
+    button = premium || null
   }
 
   // skip ad if focus is not on input boxes
